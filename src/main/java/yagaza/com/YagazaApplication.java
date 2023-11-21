dfa0473ad7ed294948f6bb6001c97b39362c5771
vpzp <sktmek11@naver.com>
2023-11-21 09:16:55 +0900

    초기 커밋

---
+++ src/main/java/yagaza/com/YagazaApplication.java
@@ -0,0 +1,13 @@
+package yagaza.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yagaza/com/YagazaApplicationTests.java
@@ -0,0 +1,13 @@
+package yagaza.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

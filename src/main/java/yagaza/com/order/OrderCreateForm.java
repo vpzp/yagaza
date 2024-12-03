fca28866ae247cf75027c9072a284a22f6f54e18
vpzp <sktmek11@naver.com>
2024-12-03 17:31:45 +0900

    푸터수정

--- src/main/java/yagaza/com/order/OrderCreateForm.java
+++ src/main/java/yagaza/com/order/OrderCreateForm.java
@@ -1,5 +1,6 @@
 package yagaza.com.order;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 public class OrderCreateForm {
     //TODO 예외값 처리 해줘야함
+//    @Digits("숫자를 입력해주세요", )
     int cash;
 
     int prod;
